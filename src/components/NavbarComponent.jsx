db8943434cd0133a4ec6924ef1d258afdb286efd
David Nodine <davidnodine@gmail.com>
2025-04-07 22:19:41 +0200

    fix dark mode nav

--- src/components/NavbarComponent.jsx
+++ src/components/NavbarComponent.jsx
@@ -1,4 +1,5 @@
 import { useRef } from "react";
+import { useTheme } from "../context/ThemeContext";
 import ThemeToggle from "./ThemeToggle";
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
@@ -6,34 +7,39 @@ import Navbar from "react-bootstrap/Navbar";
 import "../css/global.css";
 
 const NavbarComponent = () => {
+
+  const { darkMode } = useTheme();
+
   const collapseRef = useRef();
 
   const scrollTo = (id) => {
     const element = document.getElementById(id);
     const collapseEl = collapseRef.current;
-  
+
     const isMobile = window.innerWidth < 768;
     const wasExpanded = collapseEl?.classList.contains("show");
-  
+
     if (wasExpanded) {
       collapseEl.classList.remove("show");
     }
 
-    setTimeout(() => {
-      if (element) {
-        const yOffset = isMobile ? -50 : -90;
-        const y =
-          element.getBoundingClientRect().top + window.pageYOffset + yOffset;
-        window.scrollTo({ top: y, behavior: "smooth" });
-      }
-    }, wasExpanded ? 350 : 0);
+    setTimeout(
+      () => {
+        if (element) {
+          const yOffset = isMobile ? -50 : -90;
+          const y =
+            element.getBoundingClientRect().top + window.pageYOffset + yOffset;
+          window.scrollTo({ top: y, behavior: "smooth" });
+        }
+      },
+      wasExpanded ? 350 : 0
+    );
   };
-  
 
   const elements = ["about", "career", "education", "skills", "contact"];
 
   return (
-    <Navbar expand="lg" className="bg-body-tertiary sticky-top">
+    <Navbar expand="lg" className={`bg-body-tertiary sticky-top ${darkMode ? "navbar-dark" : ""}`}>
       <Container>
         <Navbar.Brand onClick={() => scrollTo("header")}>
           David Nodine
